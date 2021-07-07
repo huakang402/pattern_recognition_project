e5db2885dd713bfcc9362a5bd137032137c1f0e5
HuaKang Huang <1003970864@qq.com>
2021-07-07 18:21:12 +0800

    Fix can not read 2412 and 2416

--- preprocess/format.m
+++ preprocess/format.m
@@ -8,6 +8,9 @@ for i = 1223:5222
    if(fid ~= -1)
        I = fread(fid);
        h = imagesc(reshape(I, 128, 128)');
+       if(i == 2412 || i == 2416)
+           h = imagesc(reshape(I, 512, 512)');
+       end
        map = colormap(gray(256));
        saveas(h, strcat('../data/jpg/', ch, '.jpg'));
        fclose(fid);

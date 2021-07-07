4060bb2b9aa4ff3df90f80bee3dad826c5e62828
HuaKang Huang <1003970864@qq.com>
2021-07-07 20:09:54 +0800

    Add print process run time

--- src/main.py
+++ src/main.py
@@ -1,10 +1,14 @@
 import classifier
+import time
 
 if __name__ == '__main__':
+    start_time = time.time()
     data_train_dim, data_test_dim, label_train, label_test = classifier.load_data()
     predictedLabel, score = classifier.classifier(data_train_dim, data_test_dim, label_train, label_test)
+    end_time = time.time()
     print('------------------------------------------------')
     print('--- Use', classifier.config.get('dim_reduction_algorithm'), 'and ', end='')
     print(classifier.config.get('classifier_algorithm'), 'algorithm. ---')
     print('--- Accuracy is ', score, ' ----')
+    print('--- Run time is ', end_time - start_time, 's ----')
 
